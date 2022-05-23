5300c413e70ab4ae31df7436ea3901bc616ec220
ritikasantara <rits.santara@gmail.com>
2022-05-23 14:03:07 +0530

    keyerror handling mechanism added

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 import pandas
 
@@ -7,9 +5,9 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 #TODO 1. Create a dictionary in this format:
 
+
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
-
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
